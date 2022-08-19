432fb21b0fc7c6710dfd86ab5ae943b1e111f0c1
yu <tyslenko94@gmail.com>
2022-08-19 17:05:19 +0300

    minor final  changes

--- src/components/table.js
+++ src/components/table.js
@@ -9,26 +9,57 @@ export default function Table(){
     let filteredLaureatesState = (searchVal === "" ) ? laureatesState : laureatesState.filter((item) => item.firstname.toLowerCase().includes(searchVal.toLowerCase()))
     console.log(filteredLaureatesState)
 
-    useEffect(() =>{
-      axios.get('https://api.nobelprize.org/v1/laureate.json')
-          .then((response) => {
-            for(let i = 0; i <=100; i++){
-              let result = response.data.laureates[i];
-              setLaureatesState(laureatesState => [...laureatesState, {
-                "id": `${result.id}`,
-                "firstname": `${result.firstname}`,
-                "surname": `${result.surname}`,
-                "born": `${result.born}`,
-                "died": `${result.died}`,
-                }]);
-            }
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-    },[])
 
-    // console.log(laureatesState)
+    useEffect(() =>{
+        axios.get('https://api.nobelprize.org/v1/laureate.json')
+            .then((response) => {
+              let finalArrays = [];  
+              for(let i = 0; i < 100; i++){
+                let result = response.data.laureates[i];
+                finalArrays.push({
+                  "id": `${result.id}`,
+                  "firstname": `${result.firstname}`,
+                  "surname": `${result.surname}`,
+                  "born": `${result.born}`,
+                  "died": `${result.died}`,
+                  }) ;
+                }
+              console.log(finalArrays)
+              setLaureatesState(finalArrays)
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+      },[])
+    
+    
+    
+    
+    
+    
+    
+    
+    // useEffect(() =>{
+    //   axios.get('https://api.nobelprize.org/v1/laureate.json')
+    //       .then((response) => {
+    //         for(let i = 0; i < 100; i++){
+    //           let result = response.data.laureates[i];
+    //           setLaureatesState(laureatesState => [...laureatesState, {
+    //             "id": `${result.id}`,
+    //             "firstname": `${result.firstname}`,
+    //             "surname": `${result.surname}`,
+    //             "born": `${result.born}`,
+    //             "died": `${result.died}`,
+    //             }]);
+                
+    //         }
+    //       })
+    //       .catch((error) => {
+    //         console.log(error)
+    //       })
+    // },[])
+
+    console.log(laureatesState)
     return(
         <div>
             <input type="search" onChange={(e) => setSearchVal(e.target.value)} placeholder="Search..."/>
